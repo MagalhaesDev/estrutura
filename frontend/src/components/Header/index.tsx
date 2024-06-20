49046c64e1a74dbb859406c7ab6a88f8cd5ee924
MagalhaesDev <mateusmagalhaesemidio@gmail.com>
2024-06-20 16:22:05 -0300

    finished day task job

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,16 +1,27 @@
+import { CircleDollarSign, Building, BarChart3 } from "lucide-react"
+
 export function Header() {
   return (
     <header className="py-7 flex items-center justify-center">
       <nav>
         <ul className="flex gap-10 font-medium">
-          <li>
-            <a href="#" className="hover:text-orange-500 transition all .3s">Inicio</a>
+          <li className="flex gap-2">
+            <div className="text-green-500">
+              <BarChart3 />
+            </div>
+            <a href="#" className="hover:text-green-600 transition all .3s">Inicio</a>
           </li>
-          <li>
-            <a href="#" className="hover:text-orange-500 transition all .3s">Serviços</a>
+          <li className="flex gap-2">
+            <div className="text-red-500">
+              <Building />
+            </div>
+            <a href="#" className="hover:text-green-600 transition all .3s">Serviços</a>
           </li>
-          <li>
-            <a href="#" className="hover:text-orange-500 transition all .3s">Orçamentos</a>
+          <li className="flex gap-2">
+            <div className="text-yellow-500">
+              <CircleDollarSign />
+            </div>
+            <a href="#" className="hover:text-green-600 transition all .3s">Orçamentos</a>
           </li>
         </ul>
       </nav>
--- frontend/src/pages/Home/components/TableServices/index.tsx
+++ frontend/src/pages/Home/components/TableServices/index.tsx
@@ -2,9 +2,9 @@
 
 export function TableServices() {
     return (
-        <table className="w-11/12">
+        <table className="w-11/12 shadow-xl mb-24">
             <thead className='text-zinc-100'>
-                <tr className="bg-green-700 text-sm ">
+                <tr className="bg-green-600 text-sm ">
                     <th className="font-semibold w-[14%] rounded-tl-lg py-3">Nome</th>
                     <th className="font-semibold w-[14%]">Titulo</th>
                     <th className="font-semibold w-[14%] ">Localização</th>
@@ -14,7 +14,7 @@ export function TableServices() {
                 </tr>
             </thead>
             <tbody className='text-zinc-800 cursor-pointer text-sm'>
-                <tr className="bg-zinc-100 hover:bg-zinc-400 transition all 3s">
+                <tr className="bg-zinc-100 hover:bg-zinc-300 transition all 3s">
                     <td className="w-[14%] text-center py-2">Natalia</td>
                     <td className="w-[14%] text-center">Fios no chão</td>
                     <td className="w-[14%] text-center">Acabamento Life</td>
@@ -24,9 +24,9 @@ export function TableServices() {
                         </span>
                     </td>
                     <td className="w-[14%] text-center">Elétrica</td>
-                    <td className="w-[14%] text-center">Pendente</td>
+                    <td className="w-[14%] text-center text-red-500">Pendente</td>
                 </tr>
-                <tr className="bg-zinc-200  hover:bg-zinc-400 transition all 3s">
+                <tr className="bg-zinc-200  hover:bg-zinc-300 transition all 3s">
                     <td className="w-[14%] text-center py-2">Mateus</td>
                     <td className="w-[14%] text-center">Placa do teto</td>
                     <td className="w-[14%] text-center">Administrativo</td>
@@ -36,9 +36,9 @@ export function TableServices() {
                         </span>
                     </td>
                     <td className="w-[14%] text-center">Infraestrutura</td>
-                    <td className="w-[14%] text-center">Pendente</td>
+                    <td className="w-[14%] text-center text-red-500">Pendente</td>
                 </tr>
-                <tr className="bg-zinc-100  hover:bg-zinc-400 transition all 3s">
+                <tr className="bg-zinc-100  hover:bg-zinc-300 transition all 3s">
                     <td className="w-[14%] text-center py-2">Rafael</td>
                     <td className="w-[14%] text-center">Ventilador sem proteção</td>
                     <td className="w-[14%] text-center">Produção Woodpel</td>
@@ -48,9 +48,9 @@ export function TableServices() {
                         </span>
                     </td>
                     <td className="w-[14%] text-center">Cleyton</td>
-                    <td className="w-[14%] text-center">Pendente</td>
+                    <td className="w-[14%] text-center text-yellow-500">Aguardando serviço</td>
                 </tr>
-                <tr className="bg-zinc-200  hover:bg-zinc-400 transition all 3s">
+                <tr className="bg-zinc-200  hover:bg-zinc-300 transition all 3s">
                     <td className="w-[14%] text-center py-2">Antonio</td>
                     <td className="w-[14%] text-center">Maquina sem segurança</td>
                     <td className="w-[14%] text-center">Produção Woodflex</td>
@@ -60,7 +60,151 @@ export function TableServices() {
                         </span>
                     </td>
                     <td className="w-[14%] text-center">Cleyton</td>
-                    <td className="w-[14%] text-center">Pendente</td>
+                    <td className="w-[14%] text-center text-green-600">Concluido</td>
+                </tr>
+                <tr className="bg-zinc-100 hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Natalia</td>
+                    <td className="w-[14%] text-center">Fios no chão</td>
+                    <td className="w-[14%] text-center">Acabamento Life</td>
+                    <td className="w-[14%] text-center ">
+                        <span className="bg-yellow-500 px-3 rounded-full text-white font-medium">
+                            Médio
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Elétrica</td>
+                    <td className="w-[14%] text-center text-red-500">Pendente</td>
+                </tr>
+                <tr className="bg-zinc-200  hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Mateus</td>
+                    <td className="w-[14%] text-center">Placa do teto</td>
+                    <td className="w-[14%] text-center">Administrativo</td>
+                    <td className="w-[14%] text-center">
+                        <span className="bg-red-700 px-3 rounded-full text-white font-medium">
+                            Alto
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Infraestrutura</td>
+                    <td className="w-[14%] text-center text-red-500">Pendente</td>
+                </tr>
+                <tr className="bg-zinc-100  hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Rafael</td>
+                    <td className="w-[14%] text-center">Ventilador sem proteção</td>
+                    <td className="w-[14%] text-center">Produção Woodpel</td>
+                    <td className="w-[14%] text-center">
+                        <span className="bg-red-700 px-3 rounded-full text-white font-medium">
+                            Alto
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Cleyton</td>
+                    <td className="w-[14%] text-center text-yellow-500">Aguardando serviço</td>
+                </tr>
+                <tr className="bg-zinc-200  hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Antonio</td>
+                    <td className="w-[14%] text-center">Maquina sem segurança</td>
+                    <td className="w-[14%] text-center">Produção Woodflex</td>
+                    <td className="w-[14%] text-center">
+                        <span className="bg-green-700 px-3 rounded-full text-white font-medium">
+                            Baixo
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Cleyton</td>
+                    <td className="w-[14%] text-center text-green-600">Concluido</td>
+                </tr>
+                <tr className="bg-zinc-100 hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Natalia</td>
+                    <td className="w-[14%] text-center">Fios no chão</td>
+                    <td className="w-[14%] text-center">Acabamento Life</td>
+                    <td className="w-[14%] text-center ">
+                        <span className="bg-yellow-500 px-3 rounded-full text-white font-medium">
+                            Médio
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Elétrica</td>
+                    <td className="w-[14%] text-center text-red-500">Pendente</td>
+                </tr>
+                <tr className="bg-zinc-200  hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Mateus</td>
+                    <td className="w-[14%] text-center">Placa do teto</td>
+                    <td className="w-[14%] text-center">Administrativo</td>
+                    <td className="w-[14%] text-center">
+                        <span className="bg-red-700 px-3 rounded-full text-white font-medium">
+                            Alto
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Infraestrutura</td>
+                    <td className="w-[14%] text-center text-red-500">Pendente</td>
+                </tr>
+                <tr className="bg-zinc-100  hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Rafael</td>
+                    <td className="w-[14%] text-center">Ventilador sem proteção</td>
+                    <td className="w-[14%] text-center">Produção Woodpel</td>
+                    <td className="w-[14%] text-center">
+                        <span className="bg-red-700 px-3 rounded-full text-white font-medium">
+                            Alto
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Cleyton</td>
+                    <td className="w-[14%] text-center text-yellow-500">Aguardando serviço</td>
+                </tr>
+                <tr className="bg-zinc-200  hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Antonio</td>
+                    <td className="w-[14%] text-center">Maquina sem segurança</td>
+                    <td className="w-[14%] text-center">Produção Woodflex</td>
+                    <td className="w-[14%] text-center">
+                        <span className="bg-green-700 px-3 rounded-full text-white font-medium">
+                            Baixo
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Cleyton</td>
+                    <td className="w-[14%] text-center text-green-600">Concluido</td>
+                </tr>
+                <tr className="bg-zinc-100 hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Natalia</td>
+                    <td className="w-[14%] text-center">Fios no chão</td>
+                    <td className="w-[14%] text-center">Acabamento Life</td>
+                    <td className="w-[14%] text-center ">
+                        <span className="bg-yellow-500 px-3 rounded-full text-white font-medium">
+                            Médio
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Elétrica</td>
+                    <td className="w-[14%] text-center text-red-500">Pendente</td>
+                </tr>
+                <tr className="bg-zinc-200  hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Mateus</td>
+                    <td className="w-[14%] text-center">Placa do teto</td>
+                    <td className="w-[14%] text-center">Administrativo</td>
+                    <td className="w-[14%] text-center">
+                        <span className="bg-red-700 px-3 rounded-full text-white font-medium">
+                            Alto
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Infraestrutura</td>
+                    <td className="w-[14%] text-center text-red-500">Pendente</td>
+                </tr>
+                <tr className="bg-zinc-100  hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Rafael</td>
+                    <td className="w-[14%] text-center">Ventilador sem proteção</td>
+                    <td className="w-[14%] text-center">Produção Woodpel</td>
+                    <td className="w-[14%] text-center">
+                        <span className="bg-red-700 px-3 rounded-full text-white font-medium">
+                            Alto
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Cleyton</td>
+                    <td className="w-[14%] text-center text-yellow-500">Aguardando serviço</td>
+                </tr>
+                <tr className="bg-zinc-200  hover:bg-zinc-300 transition all 3s">
+                    <td className="w-[14%] text-center py-2">Antonio</td>
+                    <td className="w-[14%] text-center">Maquina sem segurança</td>
+                    <td className="w-[14%] text-center">Produção Woodflex</td>
+                    <td className="w-[14%] text-center">
+                        <span className="bg-green-700 px-3 rounded-full text-white font-medium">
+                            Baixo
+                        </span>
+                    </td>
+                    <td className="w-[14%] text-center">Cleyton</td>
+                    <td className="w-[14%] text-center text-green-600">Concluido</td>
                 </tr>
             </tbody>
         </table>
